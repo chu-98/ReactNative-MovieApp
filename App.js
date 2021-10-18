6ebc3fb76edd42d077a3f89b625425c11d60c05e
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-10-18 15:57:18 +0900

    AppLoading #1

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import AppLoading from "expo-app-loading";
+
+import React, { useState } from "react";
+import { Text } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open Door up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    // 이 부분이 로딩하고 싶은 것들은 전부 담는 부분
+    // preload가 필요한 이유
+    //1) 어떠한 이유로 켜지기 전에 API를 호출할 수 있다
+    //2) Database를 열어두는 시간이 필요할 수 있다
+    await new Promise(resolve => setTimeout(resolve, 10000));
+  };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onError={console.error}
+        onFinish={onFinish}
+      />
+    );
+  }
+  return <Text>We are done Loading!</Text>;
+}
