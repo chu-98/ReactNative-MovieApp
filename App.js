c80feee5a20ef280e38ab7592a62afb81c1c3d91
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-10-18 16:58:02 +0900

    AppLoading #2, #3

--- App.js
+++ App.js
@@ -1,27 +1,15 @@
 import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { useFonts } from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
 
-import React, { useState } from "react";
+import React from "react";
 import { Text } from "react-native";
 
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    // 이 부분이 로딩하고 싶은 것들은 전부 담는 부분
-    // preload가 필요한 이유
-    //1) 어떠한 이유로 켜지기 전에 API를 호출할 수 있다
-    //2) Database를 열어두는 시간이 필요할 수 있다
-    await new Promise(resolve => setTimeout(resolve, 10000));
-  };
-
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onError={console.error}
-        onFinish={onFinish}
-      />
-    );
+  const [loaded] = Font.useFonts(Ionicons.font);
+  if (!loaded) {
+    return <AppLoading />;
   }
   return <Text>We are done Loading!</Text>;
 }
