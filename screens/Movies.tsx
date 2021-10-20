fb196f86cb5d9e04ea361802dd921fd2bd9706c9
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-10-20 15:32:33 +0900

    #3.1 Structure

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,23 +1,5 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import styled from "styled-components/native";
 
-const Btn = styled.TouchableOpacity`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: ${props => props.theme.mainBgColor};
-`;
-
-const Title = styled.Text`
-  color: ${props => props.theme.textColor};
-`;
-
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
-  navigation,
-}) => (
-  <Btn onPress={() => navigation.navigate("Stack", { screen: "Three" })}>
-    <Title>Movies</Title>
-  </Btn>
-);
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {};
 export default Movies;
