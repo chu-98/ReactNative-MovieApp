58b4dedbbab4fe8a870ef0f17bf21dbd1f2665f1
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-10-21 16:17:12 +0900

    #3.3 Movies Screen (2)

--- navigation/Tabs.tsx
+++ navigation/Tabs.tsx
@@ -18,6 +18,7 @@ export default function Tabs() {
 
   return (
     <Tab.Navigator
+      sceneContainerStyle={{ backgroundColor: isDark ? BLACK : "white" }}
       screenOptions={{
         tabBarStyle: {
           backgroundColor: isDark ? BLACK : "white",
--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,39 +1,69 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React from "react";
-import { Dimensions } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Dimensions, StyleSheet } from "react-native";
+import { BlurView } from "expo-blur";
 
 import styled from "styled-components/native";
 import Swiper from "react-native-web-swiper";
+import { makeImgPath } from "../utils";
 
-const Container = styled.ScrollView`
-  background-color: ${props => props.theme.mainBgColor};
+const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
 `;
-
+const Container = styled.ScrollView``;
 const View = styled.View`
   flex: 1;
 `;
+const BgImg = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`;
+const Title = styled.Text``;
+
 const API_KEY = "a695e0cfa850fe04e3cd07cd0027d795";
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
-  const getNowPlaying = () => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
-    );
+  const [loading, setLoading] = useState(true);
+  const [nowPlaying, setNowPlaying] = useState([]);
+  useEffect(() => {
+    getNowPlaying();
+  }, []);
+
+  const getNowPlaying = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+      )
+    ).json();
+    console.log(results);
+    setNowPlaying(results);
+    setLoading(false);
   };
-  return (
+
+  return loading ? (
+    <Loader>
+      <ActivityIndicator size="large" />
+    </Loader>
+  ) : (
     <Container>
       <Swiper
         loop
-        timeout={4}
+        timeout={3}
         controlsEnabled={false}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
-        <View style={{ backgroundColor: "red" }}></View>
-        <View style={{ backgroundColor: "blue" }}></View>
-        <View style={{ backgroundColor: "red" }}></View>
-        <View style={{ backgroundColor: "blue" }}></View>
+        {nowPlaying.map(movie => (
+          <View key={movie.id}>
+            <BgImg source={{ uri: makeImgPath(movie.backdrop_path) }} />
+            <BlurView intensity={70} style={StyleSheet.absoluteFill}>
+              <Title>{movie.original_title}</Title>
+            </BlurView>
+          </View>
+        ))}
       </Swiper>
     </Container>
   );
