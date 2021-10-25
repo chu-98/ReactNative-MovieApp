4a4b29979469aab7941fd77928fe27ff4401ee84
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-10-25 15:56:17 +0900

    #3.18~19 Search Logic

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,9 +1,53 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Alert } from "react-native";
+import { useQuery } from "react-query";
+import styled from "styled-components/native";
+import { moviesAPI, TvAPI } from "../api";
 
-const Search = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text>Search</Text>
-  </View>
-);
+const Container = styled.ScrollView``;
+const SearchBar = styled.TextInput`
+  background-color: #485460;
+  padding: 15px 17px;
+  border-radius: 12px;
+  width: 90%;
+  margin: 20px auto;
+`;
+
+const Search = () => {
+  const [query, setQuery] = useState("");
+  const {
+    isLoading: moviesLoading,
+    data: moviesData,
+    refetch: searchMovies,
+  } = useQuery(["searchMovies", query], moviesAPI.search, {
+    enabled: false,
+  });
+  const {
+    isLoading: tvLoading,
+    data: tvData,
+    refetch: searchTV,
+  } = useQuery(["searchTV", query], TvAPI.search, {
+    enabled: false,
+  });
+
+  const onChangeText = (text: string) => setQuery(text);
+  const onSubmit = () => {
+    if (query === "") {
+      return;
+    }
+    searchMovies();
+    searchTV();
+  };
+  return (
+    <Container>
+      <SearchBar
+        placeholder="Search for Movie or TV Show"
+        placeholderTextColor="white"
+        returnKeyType="search"
+        onChangeText={onChangeText}
+        onSubmitEditing={onSubmit}
+      />
+    </Container>
+  );
+};
 export default Search;
